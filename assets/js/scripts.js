ecfc62936d5efe2940126f50d143d374254b83a2
M-Ramadan <mramadan@spark-sys.com>
2022-10-17 15:38:19 +0200

    birthday

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,83 @@
 $(function () {
-   const date = $("#date");
-   if (date) {
-      date.datepicker({
-         dateFormat: "dd-mm-yy",
-         duration: "fast",
+   // const date = $("#date");
+   // if (date) {
+   //    date.datepicker({
+   //       dateFormat: "dd-mm-yy",
+   //       duration: "fast",
+   //    });
+   // }
+   const birthDate = $(".birthdate.ar");
+   if (birthDate) {
+      birthDate.bootstrapBirthday({
+         widget: {
+            wrapper: {
+               tag: "div",
+               class: "row",
+            },
+            wrapperYear: {
+               use: true,
+               tag: "span",
+               class: "col",
+            },
+            wrapperMonth: {
+               use: true,
+               tag: "span",
+               class: "col",
+            },
+            wrapperDay: {
+               use: true,
+               tag: "span",
+               class: "col",
+            },
+            selectYear: {
+               name: "birthday[year]",
+               class: "form-control input-sm",
+            },
+            selectMonth: {
+               name: "birthday[month]",
+               class: "form-control input-sm",
+            },
+            selectDay: {
+               name: "birthday[day]",
+               class: "form-control input-sm",
+            },
+         },
+         text: {
+            dateFormat: "littleEndian",
+            year: "سنة",
+            month: "شهر",
+            day: "يوم",
+            months: {
+               short: [
+                  "يناير",
+                  "فبراير",
+                  "مارس",
+                  "ابريل",
+                  "مايو",
+                  "يونيو",
+                  "يوليو",
+                  "اغسطس",
+                  "سبتمبر",
+                  "اكتوبر",
+                  "نوفمبر",
+                  "ديسمبر",
+               ],
+               long: [
+                  "يناير",
+                  "فبراير",
+                  "مارس",
+                  "ابريل",
+                  "مايو",
+                  "يونيو",
+                  "يوليو",
+                  "اغسطس",
+                  "سبتمبر",
+                  "اكتوبر",
+                  "نوفمبر",
+                  "ديسمبر",
+               ],
+            },
+         },
       });
    }
 
@@ -13,9 +87,7 @@ $(function () {
          if ($(this).val() === "up-21") {
             $(this).parents("form").attr("action", "video.html");
          } else {
-            $(this)
-               .parents("form")
-               .attr("action", "https://youtu.be/J7SOfbFzY-E");
+            $(this).parents("form").attr("action", "https://erwifi.net/");
          }
       });
    }
